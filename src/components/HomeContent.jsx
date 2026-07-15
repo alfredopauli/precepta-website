4ed3d305b05900f248646a74bc9d46c72e21ee3a
Alfredo José de Castilhos Pauli Barbosa <alfredojcpbarbosa@gmail.com>
2026-07-15 17:09:42 -0300

    Disable RevealOnScroll for debugging

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -7,22 +7,18 @@ import '../style/RevealOnScroll.css';
 const HomeContent = () => {
   return (
     <div className="advertisement-wrapper">
-      <RevealOnScroll>
-        <div className="text-wrapper">
-          <div className="text">
-            <h1> Venha ser PRECEPTA também! </h1>
-            <h3> 
-              Escolha um professor da maior escola online de reforço de São Paulo e comece agora 
-              suas monitorias particulares.
-            </h3>
-          </div>
+      <div className="text-wrapper">
+        <div className="text">
+          <h1> Venha ser PRECEPTA também! </h1>
+          <h3> 
+            Escolha um professor da maior escola online de reforço de São Paulo e comece agora 
+            suas monitorias particulares.
+          </h3>
         </div>
-      </RevealOnScroll>
-      <RevealOnScroll>
-        <div className="image">
-          <img src={student} />
-        </div>
-      </RevealOnScroll>
+      </div>
+      <div className="image">
+        <img src={student} />
+      </div>
     </div>
     //<div className="advertisement-wrapper">
     //  <RevealOnScroll>
