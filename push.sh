f1c6b1ebdb1c399bb6fcb4656fff0aa798040560
Alfredo José de Castilhos Pauli Barbosa <alfredojcpbarbosa@gmail.com>
2026-07-15 17:44:39 -0300

    Change position to relative

---
+++ push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+read -p "Commit message: " message
+git add *
+git commit -m "$message"
+echo "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\n\n" > git push
+
