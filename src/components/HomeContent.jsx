116129a5fa39968610b7c9c60c465ae0a8dc674b
Alfredo José de Castilhos Pauli Barbosa <alfredojcpbarbosa@gmail.com>
2026-07-15 17:38:03 -0300

    Change position to static

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -7,37 +7,23 @@ import '../style/RevealOnScroll.css';
 const HomeContent = () => {
   return (
     <div className="advertisement-wrapper">
-      <div className="text-wrapper">
-        <div className="text">
-          <h1> Venha ser PRECEPTA também! </h1>
-          <h3> 
-            Escolha um professor da maior escola online de reforço de São Paulo e comece agora 
-            suas monitorias particulares.
-          </h3>
+      <RevealOnScroll>
+        <div className="text-wrapper">
+          <div className="text">
+            <h1> Venha ser PRECEPTA também! </h1>
+            <h3> 
+              Escolha um professor da maior escola online de reforço de São Paulo e comece agora 
+              suas monitorias particulares.
+            </h3>
+          </div>
         </div>
-      </div>
-      <div className="image">
-        <img src={student} />
-      </div>
+      </RevealOnScroll>
+      <RevealOnScroll>
+        <div className="image">
+          <img src={student} />
+        </div>
+      </RevealOnScroll>
     </div>
-    //<div className="advertisement-wrapper">
-    //  <RevealOnScroll>
-    //    <div className="text-wrapper">
-    //      <div className="text">
-    //        <h1> Venha ser PRECEPTA também! </h1>
-    //        <h3> 
-    //          Escolha um professor da maior escola online de reforço de São Paulo e comece agora 
-    //          suas monitorias particulares.
-    //        </h3>
-    //      </div>
-    //    </div>
-    //  </RevealOnScroll>
-    //  <RevealOnScroll>
-    //    <div className="image">
-    //      <img src={student} />
-    //    </div>
-    //  </RevealOnScroll>
-    //</div>
   );
 }
 
