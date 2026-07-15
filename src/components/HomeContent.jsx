38a118f7f84a53940e9216abfa197d3638d79fd3
Alfredo José de Castilhos Pauli Barbosa <alfredojcpbarbosa@gmail.com>
2026-07-15 17:07:03 -0300

    Disable RevealOnScroll for debugging

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -24,6 +24,24 @@ const HomeContent = () => {
         </div>
       </RevealOnScroll>
     </div>
+    //<div className="advertisement-wrapper">
+    //  <RevealOnScroll>
+    //    <div className="text-wrapper">
+    //      <div className="text">
+    //        <h1> Venha ser PRECEPTA também! </h1>
+    //        <h3> 
+    //          Escolha um professor da maior escola online de reforço de São Paulo e comece agora 
+    //          suas monitorias particulares.
+    //        </h3>
+    //      </div>
+    //    </div>
+    //  </RevealOnScroll>
+    //  <RevealOnScroll>
+    //    <div className="image">
+    //      <img src={student} />
+    //    </div>
+    //  </RevealOnScroll>
+    //</div>
   );
 }
 
